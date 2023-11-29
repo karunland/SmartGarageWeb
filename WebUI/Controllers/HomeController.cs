fe617bca5b81ecde49e8c141ea760491328ff8f6
karunland <1harunkorkmaz@gmail.com>
2023-11-29 19:08:40 +0300

    init commit

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+using System.Diagnostics;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
+
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("http://127.0.0.1:5000/flask/");
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> SendProximity(string speed)
+    {
+        try
+        {
+            var formData = new MultipartFormDataContent();
+            formData.Add(new StringContent(speed), "speed");
+
+            var response = await _httpClient.PostAsync("set/servo", formData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                return Json(true);
+            }
+            else
+            {
+                return Json(false);
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> SendGas(string gasSpeed)
+    {
+        try
+        {
+            var formData = new MultipartFormDataContent();
+            formData.Add(new StringContent(gasSpeed), "speed");
+
+            var response = await _httpClient.PostAsync("set/servo", formData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                return Json(true);
+            }
+            else
+            {
+                return Json(false);
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+
+        return View();
+    }
+}
