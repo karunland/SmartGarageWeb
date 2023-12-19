5c1e67fca1aaeb68718a59ca1e12b21b12f11454
karunland <1harunkorkmaz@gmail.com>
2023-12-19 17:58:20 +0300

    update frontend

--- WebUI/wwwroot/js/site.js
+++ WebUI/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function OnAjaxSuccess(result) {
+    debugger;
+    Swal.fire(
+        result.isSuccess === true ? 'Başarılı' : 'Hata',
+        result.message,
+        result.isSuccess === true ? 'success' : 'error',
+        2000
+    );
+}
+
+function OnAjaxError(result) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        timer: 2000,
+        html: result.responseJSON.message.join("<br>")
+    })
+    $(".btn[type='submit']").removeAttr("disabled", "disabled");
+}
